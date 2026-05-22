dbeef3718cd2a5ef373732a822a62ede0a93ade1
sara charafi <elkbircharafi8@gmail.com>
2026-05-22 12:54:28 +0100

    feat: add Technology enum with supported categories

---
+++ src/Enums/Technology.php
@@ -0,0 +1,12 @@
+<?php
+
+enum Technology: string
+{
+    case HTML = "HTML";
+    case CSS = "CSS";
+    case PHP = "PHP";
+    case JAVA = "JAVA";
+    case SQL = "SQL";
+    case TAILWIND = "TAILWIND";
+    case JAVASCRIPT = "JAVASCRIPT";
+}
